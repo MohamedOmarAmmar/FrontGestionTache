a97fa36b4274aebc9e60642e20f557a7f050d93f
Mohamed Omar <72110321+MohamedOmarAmmar@users.noreply.github.com>
2021-01-28 23:09:40 -0100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/front'));
+app.use(express.static('./dist/FrontGestionTache'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/front/'}),
+    res.sendFile('index.html', {root: 'dist/FrontGestionTache/'}),
 );
 
 app.listen(process.env.PORT || 8080);
